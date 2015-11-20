0e5961bd884100798148217fe1b907478da0a353
Carlos Carvallo <erotomania669@gmail.com>
2015-11-20 03:42:10 -0300

    more files added

--- v2/wp-content/themes/redlecetheme/header.php
+++ v2/wp-content/themes/redlecetheme/header.php
@@ -5,7 +5,24 @@
 		<title>Awesome Theme</title>
 		<?php wp_head(); ?>
 	</head>
-	
+
 	<body>
-		
-		<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
+		<nav class="white" role="navigation">
+      <div class="nav-wrapper container">
+        <a id="logo-container" href="index.html" class="brand-logo"> <img src="<?php bloginfo('template_url')?>/images/logon.jpg" alt="Logo"> </a>
+
+          <?php
+            wp_nav_menu(array(
+              'theme_location' => 'header-menu',
+              'container' => false,
+              'menu_class' => 'right hide-on-med-and-down',
+              )
+            );
+          ?>
+
+        <ul id="nav-mobile" class="side-nav">
+          <li><a href="#">Navbar Link</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
