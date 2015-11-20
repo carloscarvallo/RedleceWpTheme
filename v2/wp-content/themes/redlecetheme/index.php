5fadc7868f00e7653e880012d251b08b587637a5
Carlos Carvallo <erotomania669@gmail.com>
2015-11-20 04:56:47 -0300

    index change

--- v2/wp-content/themes/redlecetheme/index.php
+++ v2/wp-content/themes/redlecetheme/index.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
 
-	<h1>This is my index</h1>
+	<?php
+
+	if( have_posts() ):
+
+		while( have_posts() ): the_post(); ?>
+
+			<h3><?php the_title(); ?></h3>
+			<small>Posted on: <?php the_time(); ?> en <?php the_category(); ?></small>
+
+			<p><?php the_content(); ?></p>
+
+			<hr>
+
+		<?php endwhile;
+
+	endif;
+
+	?>
 
 <?php get_footer(); ?>
