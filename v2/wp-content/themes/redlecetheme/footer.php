ff4d4eb5146ba061884a476826ac6036c416d70a
Carlos Carvallo <erotomania669@gmail.com>
2015-11-20 05:09:09 -0300

    footer

--- v2/wp-content/themes/redlecetheme/footer.php
+++ v2/wp-content/themes/redlecetheme/footer.php
@@ -1,10 +1,21 @@
-	
-	<footer>
-		<p>This is my footer</p>
-		<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-	</footer>
-	
-	<?php wp_footer(); ?>
-	
-	</body>
+
+			<footer id="contacto" class="page-footer cremita-claro section scrollspy">
+				<?php wp_footer(); ?>
+		      <div class="row">
+		        <div class="col l3 s12">
+		          <img class="footer-img" src="<?php bloginfo('template_url')?>/images/1.png">
+		        </div>
+		        <div class="col l4 s12">
+		          <a href="http://www.oei.es/">
+		            <img class="footer-img" src="<?php bloginfo('template_url')?>/images/2.png">
+		          </a>
+		        </div>
+		        <div class="col l3 s12 center offset-l2">
+		          <h6 class="right-align red-text text-accent-4">Red Latinoamericana de Educación en Contextos de Encierro</h6>
+		          <p class="right-align red-text text-accent-4 footer">Humaitá 525 c/ 14 de mayo <br> Teléfono (+595 21) 450 903/5</p>
+		        </div>
+		      </div>
+		  </footer>
+
+  </body>
 </html>
