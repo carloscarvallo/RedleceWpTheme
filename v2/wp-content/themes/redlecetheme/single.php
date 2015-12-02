a68aa7d6f6ab7e2ed84d8d23914757d542f49a3d
Carlos Carvallo <erotomania669@gmail.com>
2015-12-01 21:03:40 -0300

    single page

---
+++ v2/wp-content/themes/redlecetheme/single.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+	<?php
+
+	if( have_posts() ):
+
+		while( have_posts() ): the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <?php the_title( '<h1 class="custom-class">','</h1>' ); ?>
+
+        <?php if( has_post_thumbnail() ): ?>
+
+          <div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
+
+        <?php endif; ?>
+
+        <small><?php the_category(); ?></small>
+
+
+        <?php the_content(); ?>
+
+      </article>
+
+			<hr>
+
+		<?php endwhile;
+
+	endif;
+
+	?>
+
+<?php get_footer(); ?>
