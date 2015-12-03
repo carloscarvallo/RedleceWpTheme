a68cc68e7e949d5142c2edcee9f84b9aaca1c980
Carlos Carvallo <erotomania669@gmail.com>
2015-12-03 04:22:27 -0300

    page titulo maqueta seguro

--- v2/wp-content/themes/redlecetheme/page-title.php
+++ v2/wp-content/themes/redlecetheme/page-title.php
@@ -16,8 +16,7 @@ get_header(); ?>
       <div class="container">
       <div class="row">
         <div class="col s12 center">
-          <h4>RedLECE es la Primera Red Regional Intergubernamental de Cooperación sobre Educación en Contextos de Encierro</h4>
-          <p class="center-align">Surge en el marco del proyecto EUROsociAL-Educación durante el III Foro Educativo Mercosur en Brasil, en noviembre de 2006, financiada por la Comisión Europea y coordinada por el Centro Internacional de Estudios Pedagógicos (CIEP). Este foro se centró en el análisis, intercambio y cooperación técnica entre países latinoamericanos en el ámbito de la educación en contextos de encierro.</p>
+          <h4><?php the_field( 'heading' ); ?></h4>
         </div>
       </div>
       </div>
@@ -35,55 +34,14 @@ get_header(); ?>
       </div>
     </div>
 
-			<?php// the_content(); ?>
-
-      <?php// the_title(); ?>
-
-<!-- =========================================================================== -->
-<!-- HARDCODING -->
-<!-- =========================================================================== -->
-
 			<div class="section white">
 				<div class="container margin">
 					<div class="row">
 						<div class="col s12">
 
-								<h5 class="center red-text text-accent-4">Arumendis si aut aut eicitassunt quiam apisquam lautae peria inti voloribus moluptatem veriorerovit que cus nonsequ atemporibus. Tem et plistiumquas unt, ut iliscid elique volorio everibusam, sit eumendis si utem. Henis aspidis atiam accus estetus evellant volore volorestibus</h5>
-								<div class="container margin">
-								<p class="left-align">El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo de la Vida El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo de la VidaHarunt acesequatis dolorat ibusamus que nobit, isci dent ipis porum lam que seque necereprem facculparum qui distotat quas dernam volupiendam que verum qui ommoEqui bero voles suntem id qui il magnis es soluptam et aut andi nem aut facessi molupta culpa volores aut ommodi suscium volorem qui offic tectur ratet, voluptas aute nonseditibus esere coreped quas doloriat recae dicatum aut ea volorer ferumqui ullupta spidero
-
-								El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo de la Vida El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo</p>
-								<br><br>
-								</div>
+							<?php the_content(); ?>
 
 						</div>
-
-						<div class="col s3 image">
-							<img class="responsive-img" src="v1/images/imagen1.jpg" alt="">
-						</div>
-						<div class="col s3 image">
-							<img class="responsive-img" src="v1/images/imagen2-1.jpg" alt="">
-						</div>
-						<div class="col s3 image">
-							<img class="responsive-img" src="v1/images/imagen3.jpg" alt="">
-						</div>
-						<div class="col s3 image">
-							<img class="responsive-img" src="v1/images/imagen1.jpg" alt="">
-						</div>
-
-						<div class="col s12">
-								<div class="container margin">
-								<br>
-								<p class="left-align">El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo de la Vida El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo de la VidaHarunt acesequatis dolorat ibusamus que nobit, isci dent ipis porum lam que seque necereprem facculparum qui distotat quas dernam volupiendam que verum qui ommoEqui bero voles suntem id qui il magnis es soluptam et aut andi nem aut facessi molupta culpa volores aut ommodi suscium volorem qui offic tectur ratet, voluptas aute nonseditibus esere coreped quas doloriat recae dicatum aut ea volorer ferumqui ullupta spidero
-
-								El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo de la Vida El nuevo Plan Iberoamericano de Alfabetización y Aprendizaje a lo Largo</p>
-								<br><br>
-								</div>
-						</div>
-
-
-<!-- ========================================================================= -->
-
 
 		<?php endwhile;
 
@@ -98,18 +56,6 @@ get_header(); ?>
 		  <br><br>
 	<?php
 
-	//loop para los ultimos post de cada categoria
-	/*$args_cat = array(
-		'include' => '1, 5, 6'
-	);
-
-	$categories = get_categories($args_cat);
-	//var_dump($categories);
-	foreach($categories as $category):
-
-	endforeach;*/
-
-	//MOSTRARA SOLO UNA CATEGORIA
 	$args = array(
 		'type' => 'post',
 		'posts_per_page' => 2,
@@ -121,8 +67,6 @@ get_header(); ?>
 	if( $lastBlog->have_posts() ):
 
 		while( $lastBlog->have_posts() ): $lastBlog->the_post();?>
-
-			<?php// get_template_part('content','featured'); ?>
 
 			  <?php get_template_part('content','image'); ?>
 
