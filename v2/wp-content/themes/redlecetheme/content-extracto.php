7ea75b49a62ae7695a18c1134262eac08d3a6d56
Carlos Carvallo <erotomania669@gmail.com>
2015-12-08 15:32:39 -0300

    uppercase para content extracto

--- v2/wp-content/themes/redlecetheme/content-extracto.php
+++ v2/wp-content/themes/redlecetheme/content-extracto.php
@@ -12,11 +12,11 @@
 
 <div class="col s12 m6">
 
-  <?php the_title(sprintf('<h5 class="center red-text text-accent-4"><a href="%s">', esc_url( get_permalink() ) ), '</a></h5>'); ?>
+  <?php the_title(sprintf('<h5 class="center red-text text-accent-4 uppercase-text"><a href="%s">', esc_url( get_permalink() ) ), '</a></h5>'); ?>
   <p class="dotted"></p>
 
     <?php the_excerpt(); ?>
-  <br><br>
+  <br>
   <p class="dotted center"><span class="red accent-4"><a class="white-text" href="<?php the_permalink(); ?>">VER TODOS</a></span></p>
 
 </div>
