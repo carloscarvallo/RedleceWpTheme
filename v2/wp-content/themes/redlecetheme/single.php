7acda664a599343492bb639f83d216c3e2e19370
Carlos Carvallo <erotomania669@gmail.com>
2015-12-08 15:02:54 -0300

    estilo para single noticias

--- v2/wp-content/themes/redlecetheme/single.php
+++ v2/wp-content/themes/redlecetheme/single.php
@@ -1,34 +1,92 @@
 <?php get_header(); ?>
 
+<div class="section white">
+	<div class="container margin">
+		<div class="row">
+			<div class="col s12">
+				<?php
+
+				if( have_posts() ):
+
+					while( have_posts() ): the_post(); ?>
+
+					<?php get_template_part('content','featured'); ?>
+
+				<?php endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+			?>
+		</div>
+
+
+
+		<?php the_content(); ?>
+
+		<div class="col s12">
+			<hr>
+
+			<br><br>
 	<?php
 
-	if( have_posts() ):
+	$args = array(
+		'type' => 'post',
+		'posts_per_page' => 2
+	);
 
-		while( have_posts() ): the_post(); ?>
+	$lastBlog = new WP_Query( $args );
 
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	if( $lastBlog->have_posts() ):
 
-        <?php the_title( '<h1 class="custom-class">','</h1>' ); ?>
+		while( $lastBlog->have_posts() ): $lastBlog->the_post();?>
 
-        <?php if( has_post_thumbnail() ): ?>
-
-          <div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
-
-        <?php endif; ?>
-
-        <small><?php the_category(); ?></small>
-
-
-        <?php the_content(); ?>
-
-      </article>
-
-			<hr>
+				<?php get_template_part('content','image'); ?>
 
 		<?php endwhile;
 
 	endif;
 
+	wp_reset_postdata();
+
 	?>
+	</div>
+
+
+	</div>
+ </div>
+</div>
+
+<div class="section cremita">
+	<div class="container margin">
+		<div class="row">
+
+			<?php
+
+			$args = array(
+				'post_type' => 'page',
+				'posts_per_page' => 2,
+				'post__in' => array(10,14)
+			);
+
+			$lastBlog = new WP_Query( $args );
+
+			if( $lastBlog->have_posts() ):
+
+				while( $lastBlog->have_posts() ): $lastBlog->the_post();?>
+
+					  <?php get_template_part('content','extracto'); ?>
+
+				<?php endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+
+			?>
+
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
