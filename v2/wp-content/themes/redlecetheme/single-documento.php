23637891bb1e2fdeba90e965b41881f5f01dc6b6
Carlos Carvallo <erotomania669@gmail.com>
2015-12-14 22:36:39 -0300

    formato para single documento

--- v2/wp-content/themes/redlecetheme/single-documento.php
+++ v2/wp-content/themes/redlecetheme/single-documento.php
@@ -1,34 +1,136 @@
 <?php get_header(); ?>
-<h1>soy un documento</h1>
-	<?php
 
-	if( have_posts() ):
+  <?php
+  // 10 id de la pagina pagina Red
+  $args = array(
+    'post_type' => 'page',
+    'posts_per_page' => 1,
+    'post__in' => array(10)
+  );
 
-		while( have_posts() ): the_post(); ?>
+  $lastBlog = new WP_Query( $args );
 
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  if( $lastBlog->have_posts() ):
 
-        <?php the_title( '<h1 class="custom-class">','</h1>' ); ?>
+    while( $lastBlog->have_posts() ): $lastBlog->the_post();?>
 
-        <?php if( has_post_thumbnail() ): ?>
+    <div class="section anaranjadito">
+      <div class="container">
+      <div class="row">
+        <div class="col s12 center">
+          <h4><?php the_field( 'heading' ); ?></h4>
+        </div>
+      </div>
+      </div>
+    </div>
 
-          <div class="pull-right"><?php the_post_thumbnail('full'); ?></div>
+    <?php endwhile;
 
-        <?php endif; ?>
+  endif;
 
-        <small><?php the_category(); ?></small>
+  wp_reset_postdata();
+
+  ?>
+
+<div class="section">
+
+  <div class="row">
+    <div class="col s2 offset-s1">
+			<br>
+			<!-- 8 id de categoria documento -->
+			<?php
+			$args = array(
+				'post_type' => 'documento',
+				'category__in' => array( 8 ),
+        'orderby' => 'title',
+        'order' => 'ASC'
+			);
+			$my_query = new WP_Query($args);
+
+		  while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+				<table class="bordered highlight">
+					<tbody>
+						<tr><td>
+							<a href="<?php the_permalink() ?>">
+								<?php the_title(); ?></a>
+						</td></tr>
+					</tbody>
+				</table>
+
+				<?php endwhile;
+				wp_reset_postdata();
+				?>
+		</div>
+
+  <div class="col s7 offset-s1">
+    <?php
+
+  	if( have_posts() ):
+
+  		while( have_posts() ): the_post(); ?>
+
+  			  <?php get_template_part('content','card'); ?>
+
+  		<?php endwhile;
+
+  	endif;
+
+  	?>
+  </div>
 
 
-        <?php the_content(); ?>
+<?php
 
-      </article>
+if( have_posts() ):
 
-			<hr>
+  while( have_posts() ): the_post(); ?>
 
-		<?php endwhile;
+          <div class="col s7 offset-s4">
 
-	endif;
+            <?php the_content(); ?>
 
-	?>
+          </div>
+        </div>
+      </div>
+
+  <?php endwhile;
+
+endif;
+
+?>
+
+
+<div class="section cremita">
+	<div class="container margin">
+		<div class="row">
+			<!-- 10 id pagina red, 14 id pagina documentos -->
+			<?php
+
+			$args = array(
+				'post_type' => 'page',
+				'posts_per_page' => 2,
+				'post__in' => array(10,14)
+			);
+
+			$lastBlog = new WP_Query( $args );
+
+			if( $lastBlog->have_posts() ):
+
+				while( $lastBlog->have_posts() ): $lastBlog->the_post();?>
+
+					  <?php get_template_part('content','extracto'); ?>
+
+				<?php endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+
+			?>
+
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
