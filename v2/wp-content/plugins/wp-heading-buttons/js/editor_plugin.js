9fc38cda2900bf93aaaf6a39b7504054393bcaaa
Carlos Carvallo <erotomania669@gmail.com>
2015-12-15 02:40:24 -0300

    boton para subtitulo

--- v2/wp-content/plugins/wp-heading-buttons/js/editor_plugin.js
+++ v2/wp-content/plugins/wp-heading-buttons/js/editor_plugin.js
@@ -4,7 +4,7 @@
 
 	tinymce.create('tinymce.plugins.WPHeadingButtons', {
 		init : function(ed, url) {
-			ed.addButton('heading1', {
+			/*ed.addButton('heading1', {
 				title : 'wpheadingbuttons.heading1',
 				image : url+'/../images/btn_h1.png',
 				onclick : function() {
@@ -24,9 +24,9 @@
 				onclick : function() {
 					ed.execCommand('FormatBlock', false, 'h3');
 				}
-			});
+			});*/
 			ed.addButton('heading4', {
-				title : 'wpheadingbuttons.heading4',
+				title : 'SUBTITULO',
 				image : url+'/../images/btn_h4.png',
 				onclick : function() {
 					ed.execCommand('FormatBlock', false, 'h4');
@@ -38,14 +38,14 @@
 				onclick : function() {
 					ed.execCommand('FormatBlock', false, 'h5');
 				}
-			});
+			});/*
 			ed.addButton('heading6', {
 				title : 'wpheadingbuttons.heading6',
 				image : url+'/../images/btn_h6.png',
 				onclick : function() {
 					ed.execCommand('FormatBlock', false, 'h6');
 				}
-			});
+			});*/
 
 		},
 
