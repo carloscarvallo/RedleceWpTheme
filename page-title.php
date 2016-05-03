5bddc08c04f661bd2edcb9448b7bb2b239b555df
Carlos Carvallo <erotomania669@gmail.com>
2016-05-03 14:48:45 -0400

    feat(content-resume.php): template part for resume

--- page-title.php
+++ page-title.php
@@ -84,36 +84,6 @@ get_header(); ?>
 
 </div>
 
-<div class="section cremita">
-	<div class="container margin">
-		<div class="row">
-
-			<?php
-
-			$args = array(
-				'post_type' => 'page',
-				'posts_per_page' => 2,
-				'post__in' => array(10,14)
-			);
-
-			$lastBlog = new WP_Query( $args );
-
-			if( $lastBlog->have_posts() ):
-
-				while( $lastBlog->have_posts() ): $lastBlog->the_post();?>
-
-					  <?php get_template_part('content','extracto'); ?>
-
-				<?php endwhile;
-
-			endif;
-
-			wp_reset_postdata();
-
-			?>
-
-		</div>
-	</div>
-</div>
+<?php get_template_part('content','resume'); ?>
 
 <?php get_footer(); ?>
