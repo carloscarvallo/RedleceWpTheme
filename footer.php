ea69d10c3c34dec88d3a9e90e1587340c72ebd03
Carlos Carvallo <erotomania669@gmail.com>
2016-05-03 14:18:24 -0400

    feat(redlece.css, footer.php): inserting new logo

--- footer.php
+++ footer.php
@@ -2,16 +2,21 @@
 			<footer id="contacto" class="page-footer cremita-claro section scrollspy">
 				<?php wp_footer(); ?>
 		      <div class="row">
-		        <div class="col l3 s12">
-		          <img class="footer-img" src="<?php bloginfo('template_url')?>/images/1.png">
+		        <div class="col l3 s12 center custom-padding">
+					<a href="http://www.redlece.net">
+		        		<img class="footer-img" src="<?php bloginfo('template_url')?>/images/1.png">
+					</a>
 		        </div>
-		        <div class="col l4 s12">
-		          <a href="http://www.oei.es/">
+				<div class="col l4 s12 center custom-padding">
+		        	<a href="http://www.oei.es/">
+		        		<img class="footer-img" src="<?php bloginfo('template_url')?>/images/logo-oei-bilingue.png">
+		        	</a>
+		        </div>
+		        <div class="col l2 s12 center custom-padding">
 		            <img class="footer-img" src="<?php bloginfo('template_url')?>/images/2.png">
-		          </a>
 		        </div>
-		        <div class="col l3 s12 center offset-l2">
-		          <h6 class="right-align red-text text-accent-4">Red Latinoamericana de Educación en Contextos de Encierro</h6>
+		        <div class="col l3 s12 center custom-padding">
+		          <h6 class="right-align red-text text-accent-4">Red Latinoamericana de Educación <br> en Contextos de Encierro</h6>
 		          <p class="right-align red-text text-accent-4 footer">Humaitá 525 c/ 14 de mayo <br> Teléfono (+595 21) 450 903/5</p>
 		        </div>
 		      </div>
